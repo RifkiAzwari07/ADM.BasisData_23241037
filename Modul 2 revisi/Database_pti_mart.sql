f911368b2d655783e5305f17bbd2792f5794b943
RifkiAzwari07 <145946605+RifkiAzwari07@users.noreply.github.com>
2025-06-16 10:25:48 +0800

    Add files via upload

---
+++ Modul 2 revisi/Database_pti_mart.sql
@@ -0,0 +1,9 @@
+-- Membuat database baru
+CREATE DATABASE IF NOT EXISTS PTI_MART;
+
+USE PTI_MART;
+
+-- melihat detail kolom dalam tiap tabel
+DESCRIBE ms_pelanggan_dqlab;
+DESCRIBE ms_produk_dqlab;
+DESCRIBE tr_penjualan_dqlab;
